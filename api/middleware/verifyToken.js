4368dcfca6ed5c457001f883372c4223c790d6ca
V10codes <chetankumarpatel2002@gmail.com>
2024-11-07 01:23:32 +0530

    added Homepage ss

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -11,15 +11,11 @@ export const verifyToken = (req, res, next) => {
   // Verify the token using JWT secret key
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, payload) => {
     if (err) {
-      // Handle errors appropriately
       if (err.name === "JsonWebTokenError") {
-        // Invalid token format or signature
         return res.status(403).json({ message: "Invalid Token Format!" });
       } else if (err.name === "TokenExpiredError") {
-        // Expired token
         return res.status(401).json({ message: "Token Expired!" });
       } else {
-        // Other errors
         console.error("Error verifying token:", err);
         return res.status(500).json({ message: "Internal Server Error" });
       }
@@ -28,7 +24,6 @@ export const verifyToken = (req, res, next) => {
     // Token is valid, extract user ID from payload (assuming 'id' field in payload)
     req.userId = payload.id;
 
-    // Continue to the next middleware or route handler
     next();
   });
 };
